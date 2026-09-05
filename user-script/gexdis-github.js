ceb35fab9e2721fc77443cc53c9b82775da7f23d
Juraj Brabec <juraj@brabec.sk>
2026-09-05 18:02:13 +0200

    chore: update userscript version and add update URLs

--- user-script/gexdis-github.js
+++ user-script/gexdis-github.js
@@ -5,11 +5,13 @@
 // @grant       GM.xmlHttpRequest
 // @connect     nas.home
 // @connect     http://nas.home:10203
-// @version     2026-09-04
+// @version     2026-09-05
 // @author      NOiSE
 // @description Send GitHub repository artefacts to GexDis downloader
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @run-at      document-idle
+// @downloadURL https://raw.githubusercontent.com/NOiSE-GitHub/gexdis/main/user-script/gexdis-github.js
+// @updateURL   https://raw.githubusercontent.com/NOiSE-GitHub/gexdis/main/user-script/gexdis-github.js
 // ==/UserScript==
 
 (function () {
